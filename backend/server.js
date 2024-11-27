7fb5571bf0c8062f61631634377f0a293d69eda3
Ameerah <ameeraharis03@gmail.com>
2024-11-27 11:09:07 +0400

    Got the collection to the server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ let db;
 MongoClient.connect('mongodb+srv://ameeraharis369:pac30nov@cluster0.kyxtfvq.mongodb.net', (err, client) => {
 db = client.db('webstore')
 })
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    next();
+  });
     
 app.get('/collection/lessons', async (req, res,next) => {
     try {
