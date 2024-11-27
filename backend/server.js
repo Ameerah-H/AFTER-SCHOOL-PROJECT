db923de970fda3603466a777716fcd5fbe4e07f7
Ameerah <ameeraharis03@gmail.com>
2024-11-27 11:12:48 +0400

    Added the fetch and brought it to front end

--- backend/server.js
+++ backend/server.js
@@ -30,59 +30,6 @@ app.get('/collection/lessons', async (req, res,next) => {
     }
 });
 
-app.param('collectionName',(req,res,next,collectionName)=>{
-    req.collection = db.collection(collectionName)
-    return next()
-})
-app.get('/collection/:collectionName', (req, res, next) => { 
-    req.collection.find({}).toArray((e, results) => { 
-    if (e) return next(e) 
-    res.send(results) 
-    }) 
-})
-//add post
-app.post('/collection/:collectionName',(req,res,next)=>{
-    req.collection.insert(req.body,(e,results)=>{
-        if (e) return next (e)
-            res.send(results.ops)
-    })
-}) 
-
-//return with object id
-
-const objectId = require('mongodb').ObjectId;
-app.get('/collection/:collectionName/:id',
-    (req,res,next)=>{
-        req.collection.findOne({_id: new objectId(req.params.id)}, (e,result) =>{
-            if (e) return next(e)
-                res.send(result)
-        })
-    })
-
-//update an object
-
-app.put('/collection/:collectionName/:id',(req,res,next)=>{
-    req.collection.update(
-        {_id: new objectId(req.params.id)},
-        {$set:req.body},
-        {safe:true, multi:false},
-        (e,result)=>{
-            if (e) return next(e)
-                res.send((result.result.n===1)?{msg:'success'}:{msg:'error'})
-        })
-})
-
-//delete 
-
-app.delete('/collection/:collectionName/:id',(req,res,next)=>{
-    req.collection.deleteOne(
-        {_id:objectId(req.params.id) },(e,result)=>{
-            if (e) return next (e)
-                res.send((result.result.n ===1 )?
-            {msg:'success'}:{msg:'error'})
-        })
-})
-
 const port = process.env.PORT||3000 
 app.listen(port,()=>{
     console.log('Express.js server running at localhost 3000')
